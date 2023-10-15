a471a29a4116a28a909a5a26e01f04b754887645
João Lima <up202108891@up.pt>
2023-10-15 18:31:12 +0100

    macros

--- Project1/base_code_P1/include/macros.h
+++ Project1/base_code_P1/include/macros.h
@@ -5,8 +5,9 @@
 #define STATE_A_RCV 2
 #define STATE_C_RCV 3
 #define STATE_DATA 4
-#define STATE_BCC_OK 5
-#define STATE_STOP 6
+#define STATE_ESC_FOUND 5
+#define STATE_BCC_OK 6
+#define STATE_STOP 7
 
 #define FLAG 0x7E
 #define ESC 0x7D
