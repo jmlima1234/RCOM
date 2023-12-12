e2a8fd3a517889c9f9fa76fd35ff6fc72ebced1f
João Lima <up202108891@up.pt>
2023-12-12 16:45:07 +0000

    compiles but doesn't run

--- Project2/code/include/application.h
+++ Project2/code/include/application.h
@@ -6,7 +6,21 @@
 #include <unistd.h>
 #include <netdb.h>
 #include <string.h>
+#include <regex.h>
 
+#define MAX_LENGTH  500
+#define FTP_PORT    21
+
+/* Server Response Codes */
+#define SERVER_READY_FOR_AUTH   220
+#define SERVER_READY_FOR_PASS   331
+#define LOGIN_SUCCESS           230
+#define SERVER_PASSIVE_MODE     227
+#define READY_FOR_DATA_TRANSFER 150
+#define TRANSFER_COMPLETED      226
+#define SERVER_GOODBYE          221
+
+/* Regular Expressions for URL Parsing */
 #define HOST      "%*[^/]//%[^/]"
 #define HOST_AT   "%*[^/]//%*[^@]@%[^/]"
 #define RESOURCE  "%*[^/]//%*[^/]/%s"
@@ -15,12 +29,30 @@
 #define RESPCODE  "%d"
 #define PASSIVE   "%*[^(](%d,%d,%d,%d,%d,%d)%*[^\n$)]"
 
-
-struct hostent {
-    char *h_name;    // Official name of the host.
-    char **h_aliases;    // A NULL-terminated array of alternate names for the host.
-    int h_addrtype;    // The type of address being returned; usually AF_INET.
-    int h_length;    // The length of the address in bytes.
-    char **h_addr_list;    // A zero-terminated array of network addresses for the host.
-    // Host addresses are in Network Byte Order.
+/* Parser output */
+struct URL {
+    char host[MAX_LENGTH];      // 'ftp.up.pt'
+    char resource[MAX_LENGTH];  // 'parrot/misc/canary/warrant-canary-0.txt'
+    char file[MAX_LENGTH];      // 'warrant-canary-0.txt'
+    char user[MAX_LENGTH];      // 'username'
+    char password[MAX_LENGTH];  // 'password'
+    char ip[MAX_LENGTH];        // 193.137.29.15
 };
+
+/* Response State Enumeration */
+typedef enum {
+    STATE_START,
+    STATE_SINGLE_LINE,
+    STATE_MULTI_LINE,
+    STATE_END
+} ResponseState;
+
+int parseFTP(char *input, struct URL *url);
+int create_socket(char *ip, int port);
+int handleAuth(const int sockfd, const char *username, const char *password);
+int enablePassiveMode(const int sockfd, char *ip, int *port);
+int getResponse(const int sockfd, char *buffer);
+int requestFile(const int sockfd, char *resource);
+int downloadData(const int sockfdControl,const int sockfdData, char *filename);
+int closeSockets(const int sockfdControl,const int sockfdData);
+void verifySocketConnection(const int sockfd, char *ip, int port);
