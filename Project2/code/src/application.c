a7cc9e18741280e94a0444b18a3d0912dce8d4d6
João Lima <up202108891@up.pt>
2023-12-13 10:14:30 +0000

    stuck in passive mode

--- Project2/code/src/application.c
+++ Project2/code/src/application.c
@@ -7,15 +7,15 @@ int parseFTP(char *input, struct URL *url) {
     if(regexec(&compare, input, 0, NULL, 0)) return -1;
 
     regcomp(&compare, "@", 0);
-    if(regexec(&compare, input, 0, NULL, 0)){
-        sscanf(input, HOST_AT, url->host);
-        sscanf(input, USER, url->host);
-        sscanf(input, PASSWORD, url->host);
-    }
-    else {
+    if(regexec(&compare, input, 0, NULL, 0) != 0){
         sscanf(input, HOST, url->host);
         strcpy(url->user, "anonymous");
         strcpy(url->password, "password");
+    }
+    else {
+        sscanf(input, HOST_AT, url->host);
+        sscanf(input, USER, url->user);
+        sscanf(input, PASSWORD, url->password);
     }
 
     //esta parte foi tirada do ficheiro getip.c
@@ -27,8 +27,7 @@ int parseFTP(char *input, struct URL *url) {
     }
     strcpy(url->ip, inet_ntoa(*((struct in_addr *) h->h_addr)));
 
-    return !(strlen(url->host) && strlen(url->user) && 
-           strlen(url->password) && strlen(url->resource) && strlen(url->file));
+    return 0;
 }
 
 int create_socket(char *ip, int port) {
@@ -67,7 +66,7 @@ int handleAuth(const int sockfd, const char *username, const char *password) {
     sprintf(cmdPass, "PASS %s\n", password);
 
     write(sockfd, cmdUser, strlen(cmdUser));
-    if (getResponse(sockfd, response) != SERVER_READY_FOR_PASS) {
+    if (getResponse(sockfd, response) != SERVER_READY_FOR_AUTH) {
         printf("Unknown user '%s'. Abort.\n", username);
         exit(-1);
     }
@@ -83,6 +82,7 @@ int enablePassiveMode(const int sockfd, char *ip, int *port) {
     write(sockfd, "PASV\n", 5);
     if (getResponse(sockfd, response) != SERVER_PASSIVE_MODE) return -1;
 
+    printf("Passei o if");
     sscanf(response, PASSIVE, &p1, &p2, &p3, &p4, &portHigh, &portLow);
     *port = portHigh * 256 + portLow;
     sprintf(ip, "%d.%d.%d.%d", p1, p2, p3, p4);
@@ -124,6 +124,7 @@ int getResponse(const int sockfd, char *buffer) {
     }
 
     sscanf(buffer, RESPCODE, &code);
+    printf("%d",code);
     return code;
 }
 
@@ -180,25 +181,26 @@ int main(int argc, char* argv[]) {
     
     struct URL application;
     memset(&application, 0, sizeof(application));
+
     if (parseFTP(argv[1], &application) != 0) {
         printf("Parse error. Usage: ./download ftp://[<user>:<password>@]<host>/<url-path>\n");
         exit(-1);
     }
-
     printf("Hostname: %s\nPath: %s\nFilename: %s\nUsername: %s\nPassword: %s\nIP Address: %s\n", application.host, application.resource,application.file,application.user,application.password,application.ip);
+
 
     int sockfdControl = create_socket(application.ip, FTP_PORT);
     char response[MAX_LENGTH];
     verifySocketConnection(sockfdControl, application.ip, FTP_PORT);
 
-    if (handleAuth(sockfdControl, application.user, application.password) != SERVER_READY_FOR_AUTH) {
+    if (handleAuth(sockfdControl, application.user, application.password) != SERVER_READY_FOR_PASS) {
         printf("Authentication failed for user '%s' with password '%s'.\n", application.user,application.password);
         exit(-1);
     }
 
     char dataIP[MAX_LENGTH];
     int dataPort;
-    if(enablePassiveMode(sockfdControl, dataIP, &dataPort)!= PASSIVE){
+    if(enablePassiveMode(sockfdControl, dataIP, &dataPort)!= SERVER_PASSIVE_MODE){
         printf("Passive mode failed\n");
         exit(-1);
     }
